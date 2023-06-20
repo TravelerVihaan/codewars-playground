3abdf0163b58858fcfdf9f66b7f31503e6fe8f0b
TravelerVihaan <lukasz.biel@10g.pl>
2023-06-20 02:59:55 +0200

    StripComments solution

--- src/main/java/com/github/vihaan/codewars/kyu4/StripComments.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/StripComments.java
@@ -1,5 +1,9 @@
 package com.github.vihaan.codewars.kyu4;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
 /*
 Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
 
@@ -27,7 +31,17 @@ var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["
 public class StripComments {
 
     public static String stripComments(String text, String[] commentSymbols) {
-        return "";
+        String[] textLines = text.split(Pattern.quote("\n"));
+        StringBuilder outputTextBuilder = new StringBuilder();
+        for (String textLine : textLines) {
+            Arrays.stream(commentSymbols)
+                    .filter(textLine::contains)
+                    .map(symbol -> textLine.substring(0, textLine.indexOf(symbol)).trim())
+                    .min(Comparator.comparing(String::length))
+                    .ifPresentOrElse(line -> outputTextBuilder.append(line.stripTrailing()).append("\n"), () -> outputTextBuilder.append(textLine.stripTrailing()).append("\n"));
+        }
+        var outputText = outputTextBuilder.toString();
+        return outputText.endsWith("\n") ? outputText.substring(0, outputText.length() - 1) : outputText;
     }
 
 }
