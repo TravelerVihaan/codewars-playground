ded29106993b525763e5babdad2c57caafc026ce
TravelerVihaan <lukasz.biel@10g.pl>
2023-06-20 23:08:42 +0200

    Codewars style ranking system

--- src/main/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/User.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/User.java
@@ -5,44 +5,64 @@ public class User {
     int progress;
 
     public User() {
-        this.rank = -8;
+        this.rank = LOWEST_RANK;
         this.progress = 0;
     }
 
     public void incProgress(int taskRank) {
-        var obtainedProgress = 0;
-        if (isMaxRank())
+        if (isMaxRank()) {
             return;
-        if (taskRank == this.rank) {
+        } else if (!isValidTaskRank(taskRank)) {
+            throw new IllegalArgumentException();
+        }
+        var obtainedProgress = 0;
+        if (rank == taskRank) {
             obtainedProgress = 3;
-        } else if (taskRank < this.rank) {
-            var diff = Math.abs(this.rank - taskRank);
-            if (Integer.signum(taskRank) != Integer.signum(this.rank)) {
+        } else if (rank > taskRank) {
+            var diff = Math.abs(rank - taskRank);
+            if (Integer.signum(rank) != Integer.signum(taskRank)) {
                 diff -= 1;
             }
-            obtainedProgress += (diff > 1 || diff < -1) ? 0 : 1;
+            obtainedProgress = diff <= 1 ? 1 : 0;
         } else {
-            var diff = taskRank - this.rank;
-            if (Integer.signum(taskRank) != Integer.signum(this.rank)) {
+            var diff = Math.abs(taskRank - rank);
+            if (Integer.signum(rank) != Integer.signum(taskRank)) {
                 diff -= 1;
             }
-            obtainedProgress = 10 * diff * diff;
+            obtainedProgress = Math.toIntExact(Math.round(10 * Math.pow(diff, 2)));
         }
         progress += obtainedProgress;
-        while (progress >= EXP_TO_PROMOTION && rank <= 8) {
+        while (progress >= EXP_TO_PROMOTION && rank < HIGHEST_RANK) {
             progress -= EXP_TO_PROMOTION;
             rank += 1;
             if (rank == 0) {
-                rank += 1;
+                rank = 1;
+            } else if (rank == HIGHEST_RANK) {
+                progress = 0;
+                return;
             }
+        }
+
+        if (rank >= HIGHEST_RANK) {
+            rank = HIGHEST_RANK;
+            progress = 0;
+        } else if (rank < LOWEST_RANK) {
+            rank = LOWEST_RANK;
+        } else if (rank == 0) {
+            rank = 1;
         }
     }
 
+    private boolean isValidTaskRank(int taskRank) {
+        return taskRank <= HIGHEST_RANK && taskRank >= LOWEST_RANK && taskRank != 0;
+    }
+
     private boolean isMaxRank() {
-        return this.rank == HIGHEST_RANK;
+        return this.rank >= HIGHEST_RANK;
     }
 
     private static final int HIGHEST_RANK = 8;
+    private static final int LOWEST_RANK = -8;
     private static final int EXP_TO_PROMOTION = 100;
 }
 
