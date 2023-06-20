d4e94d2c4a1bd80e2e1edfc3aaa06ee64fc62307
TravelerVihaan <lukasz.biel@10g.pl>
2023-06-20 19:13:39 +0200

    NextBiggerNumber

---
+++ src/main/java/com/github/vihaan/codewars/kyu4/NextBiggerNumber.java
@@ -0,0 +1,20 @@
+package com.github.vihaan.codewars.kyu4;
+
+/*
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+  12 ==> 21
+ 513 ==> 531
+2017 ==> 2071
+
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift, None in Rust):
+
+  9 ==> -1
+111 ==> -1
+531 ==> -1
+ */
+public class NextBiggerNumber {
+    public static long nextBiggerNumber(long n) {
+        return n;
+    }
+}
