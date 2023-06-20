f37d4ae6502edc3473321af9f8983ef0db9cf2cc
TravelerVihaan <lukasz.biel@10g.pl>
2023-06-20 19:31:28 +0200

    Codewars style ranking system

---
+++ src/main/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/User.java
@@ -0,0 +1,21 @@
+package com.github.vihaan.codewars.kyu4.codewarsStyleRankingSystem;
+
+public class User {
+    int rank;
+    int progress;
+
+    public User() {
+        this.rank = -8;
+        this.progress = 0;
+    }
+
+    public void incProgress(int taskRank) {
+
+    }
+
+    private static final int LOWEST_RANK = -8;
+    private static final int HIGHEST_RANK = 8;
+    private static final int EXP_TO_PROMOTION = 100;
+}
+
+
--- src/test/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/UserTest.java
+++ src/test/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/UserTest.java
@@ -1,41 +1,45 @@
 package com.github.vihaan.codewars.kyu4.codewarsStyleRankingSystem;
 
-public class UserTest {
-//    User user = new User();
-//
-//    private void do_test(int rank, int expectedRank, int expectedProgress) {
-//        if (rank != 0) user.incProgress(rank);
-//        assertEquals("After applying rank of " + rank, expectedRank, user.rank);
-//        assertEquals("After applying rank of " + rank, expectedProgress, user.progress);
-//    }
+import org.junit.jupiter.api.Test;
 
-//    @Test
-//    public void testIncProgress() throws Exception {
-//        do_test(-8, -8, 3);
-//
-//        user = new User();
-//        do_test(-7, -8, 10);
-//
-//        user = new User();
-//        do_test(-6, -8, 40);
-//
-//        user = new User();
-//        do_test(-5, -8, 90);
-//
-//        user = new User();
-//        do_test(-4, -7, 60);
-//
-//        user = new User();
-//        do_test(-8, -8, 3);
-//
-//        user = new User();
-//        do_test(1, -2, 40);
-//        do_test(1, -2, 80);
-//        do_test(1, -1, 20);
-//        do_test(1, -1, 30);
-//        do_test(1, -1, 40);
-//        do_test(2, -1, 80);
-//        do_test(2, 1, 20);
-//        do_test(-1, 1, 21);
-//    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+    User user = new User();
+
+    private void do_test(int rank, int expectedRank, int expectedProgress) {
+        if (rank != 0) user.incProgress(rank);
+        assertEquals(expectedRank, user.rank, "After applying rank of " + rank);
+        assertEquals(expectedProgress, user.progress, "After applying rank of " + rank);
+    }
+
+    @Test
+    public void testIncProgress() throws Exception {
+        do_test(-8, -8, 3);
+
+        user = new User();
+        do_test(-7, -8, 10);
+
+        user = new User();
+        do_test(-6, -8, 40);
+
+        user = new User();
+        do_test(-5, -8, 90);
+
+        user = new User();
+        do_test(-4, -7, 60);
+
+        user = new User();
+        do_test(-8, -8, 3);
+
+        user = new User();
+        do_test(1, -2, 40);
+        do_test(1, -2, 80);
+        do_test(1, -1, 20);
+        do_test(1, -1, 30);
+        do_test(1, -1, 40);
+        do_test(2, -1, 80);
+        do_test(2, 1, 20);
+        do_test(-1, 1, 21);
+    }
 }
