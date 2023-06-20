cbd8026a26dd5ee51a44064704464fe6c26b386c
TravelerVihaan <lukasz.biel@10g.pl>
2023-06-20 20:17:52 +0200

    Codewars style ranking system

--- src/main/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/User.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/codewarsStyleRankingSystem/User.java
@@ -10,10 +10,38 @@ public class User {
     }
 
     public void incProgress(int taskRank) {
-
+        var obtainedProgress = 0;
+        if (isMaxRank())
+            return;
+        if (taskRank == this.rank) {
+            obtainedProgress = 3;
+        } else if (taskRank < this.rank) {
+            var diff = Math.abs(this.rank - taskRank);
+            if (Integer.signum(taskRank) != Integer.signum(this.rank)) {
+                diff -= 1;
+            }
+            obtainedProgress += (diff > 1 || diff < -1) ? 0 : 1;
+        } else {
+            var diff = taskRank - this.rank;
+            if (Integer.signum(taskRank) != Integer.signum(this.rank)) {
+                diff -= 1;
+            }
+            obtainedProgress = 10 * diff * diff;
+        }
+        progress += obtainedProgress;
+        while (progress >= EXP_TO_PROMOTION && rank <= 8) {
+            progress -= EXP_TO_PROMOTION;
+            rank += 1;
+            if (rank == 0) {
+                rank += 1;
+            }
+        }
     }
 
-    private static final int LOWEST_RANK = -8;
+    private boolean isMaxRank() {
+        return this.rank == HIGHEST_RANK;
+    }
+
     private static final int HIGHEST_RANK = 8;
     private static final int EXP_TO_PROMOTION = 100;
 }
