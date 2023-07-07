d28a8f72ffad88c7a6c5351f7bb1ab27007ccbd8
TravelerVihaan <lukasz.biel@10g.pl>
2023-07-07 03:50:32 +0200

    Adding Big Numbers

--- src/main/java/com/github/vihaan/codewars/kyu4/AddingBigNumbers.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/AddingBigNumbers.java
@@ -1,5 +1,10 @@
 package com.github.vihaan.codewars.kyu4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /*
 We need to sum big numbers and we require your help.
 
@@ -17,6 +22,47 @@ Notes
  */
 public class AddingBigNumbers {
   public static String add(String a, String b) {
-    return a + b;
+    List<Integer> aNumbers = extractNumbers(a);
+    List<Integer> bNumbers = extractNumbers(b);
+    List<Integer> resultNumbers = new ArrayList<>();
+    List<Integer> longerNumbers = List.of(aNumbers, bNumbers).stream().max(Comparator.comparing(List::size)).orElseGet(Collections::emptyList);
+
+    int reps = Math.min(aNumbers.size(), bNumbers.size());
+    int accumulator = 0;
+    for (int i=0; i < reps; i++) {
+        int sum = aNumbers.get(i) + bNumbers.get(i) + accumulator;
+        if (sum > 10) {
+            accumulator = sum / 10;
+            resultNumbers.add(sum % 10);
+        } else {
+            accumulator = 0;
+            resultNumbers.add(sum);
+        }
+    }
+
+    for(int i=reps; i < longerNumbers.size(); i++) {
+        int sum = longerNumbers.get(i) + accumulator;
+        if (sum > 10) {
+            accumulator = sum / 10;
+            resultNumbers.add(sum % 10);
+        } else {
+            accumulator = 0;
+            resultNumbers.add(sum);
+        }
+    }
+    StringBuilder outputBuilder = new StringBuilder();
+    Collections.reverse(resultNumbers);
+    resultNumbers.forEach(outputBuilder::append);
+
+    return outputBuilder.toString();
+  }
+
+  private static List<Integer> extractNumbers(String input) {
+      char[] charNumbers = input.toCharArray();
+      List<Integer> resultNumbers = new ArrayList<>();
+      for (int i=charNumbers.length-1; i >= 0; i--) {
+          resultNumbers.add(Integer.parseInt(String.valueOf(charNumbers[i])));
+      }
+      return resultNumbers;
   }
 }
