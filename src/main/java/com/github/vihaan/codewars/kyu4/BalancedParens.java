6d75f44ea712012f3bdaad30331735bf9d675a7c
TravelerVihaan <lukasz.biel@10g.pl>
2023-07-07 04:47:32 +0200

    BalancedParens

---
+++ src/main/java/com/github/vihaan/codewars/kyu4/BalancedParens.java
@@ -0,0 +1,19 @@
+package com.github.vihaan.codewars.kyu4;
+
+import java.util.List;
+
+/*
+Write a function which makes a list of strings representing all of the ways you can balance n pairs of parentheses
+Examples
+
+balancedParens(0) returns ArrayList<String> with element:  ""
+balancedParens(1) returns ArrayList<String> with element:  "()"
+balancedParens(2) returns ArrayList<String> with elements: "()()","(())"
+balancedParens(3) returns ArrayList<String> with elements: "()()()","(())()","()(())","(()())"
+ */
+public class BalancedParens {
+    public static List<String> balancedParens (int n) {
+        // your code here
+        return null;
+    }
+}
