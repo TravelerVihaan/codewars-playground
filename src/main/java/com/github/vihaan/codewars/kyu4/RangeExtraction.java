d0548074f44205b887b5077ecf2b82c9f98c867d
Vihaan <vihaan.github@outlook.com>
2024-05-05 08:52:19 +0200

    RangeExtraction

--- src/main/java/com/github/vihaan/codewars/kyu4/RangeExtraction.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/RangeExtraction.java
@@ -6,18 +6,29 @@ class RangeExtraction {
 		int lastConsecutive = arr[0];
 		StringBuilder result = new StringBuilder(String.valueOf(arr[0]));
 		for (int i = 1; i < arr.length; i++) {
-			if (arr[i - 1] + 1 == arr[i] && (arr[i] != 0 && arr[i - 1] != 0)) {
+			if (arr[i - 1] + 1 == arr[i]) {
 				lastConsecutive = arr[i];
 				consecutiveNumbers += 1;
+				if (i == arr.length - 1) {
+					if (consecutiveNumbers > 1)
+						result.append("-").append(lastConsecutive);
+					else
+						result.append(",").append(lastConsecutive);
+					break;
+				}
 			} else {
 				if (consecutiveNumbers == 0) {
 					result.append(",");
 				}
-				if (consecutiveNumbers > 0) {
-					result//.append(lastConsecutive - consecutiveNumbers)
-							.append("-")
+				if (consecutiveNumbers > 1) {
+					result.append("-")
 							.append(lastConsecutive)
 							.append(",");
+					consecutiveNumbers = 0;
+					lastConsecutive = 0;
+					result.append(arr[i]);
+				} else if (consecutiveNumbers == 1) {
+					result.append(",").append(lastConsecutive).append(",").append(arr[i]);
 					consecutiveNumbers = 0;
 					lastConsecutive = 0;
 				} else {
