a628dc8619eb95febca2cb0a78131f23ede2e8f1
Vihaan <vihaan.github@outlook.com>
2024-05-25 23:11:54 +0200

    Hackerrank task

---
+++ src/main/java/com/github/vihaan/hackerrank/towerbreakers/Result.java
@@ -0,0 +1,19 @@
+package com.github.vihaan.hackerrank.towerbreakers;
+
+class Result {
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m) {
+    // Write your code here
+        return 0;
+    }
+
+}
