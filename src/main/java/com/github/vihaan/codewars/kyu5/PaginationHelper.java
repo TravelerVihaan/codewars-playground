f8d3426344ae0d8c8f49b61e0101ba3974d63f48
Lukasz Biel <32998032+TravelerVihaan@users.noreply.github.com>
2024-06-01 23:05:27 +0200

    PaginationHelper

--- src/main/java/com/github/vihaan/codewars/kyu5/PaginationHelper.java
+++ src/main/java/com/github/vihaan/codewars/kyu5/PaginationHelper.java
@@ -30,64 +30,65 @@ import java.util.List;
  */
 
 public class PaginationHelper<I> {
+    private final List<I> content;
+    private final int pageSize;
 
-  private final List<I> content;
-  private final int pageSize;
-
-  /**
-   * The constructor takes in an array of items and a integer indicating how many
-   * items fit within a single page
-   */
-  public PaginationHelper(List<I> collection, int itemsPerPage) {
-      this.pageSize = itemsPerPage;
-      this.content = collection;
-  }
-  
-  /**
-   * returns the number of items within the entire collection
-   */
-  public int itemCount() {
-    return content.size();
-  }
-  
-  /**
-   * returns the number of pages
-   */
-  public int pageCount() {
-      int pageCount = itemCount() / pageSize;
-      return itemCount() % pageSize == 0 ? pageCount : pageCount + 1;
-  }
-  
-  /**
-   * returns the number of items on the current page. page_index is zero based.
-   * this method should return -1 for pageIndex values that are out of range
-   */
-  public int pageItemCount(int pageIndex) {
-      if (pageIndex < 0 || itemCount() == 0 || pageIndex > pageCount()) {
-          return -1;
-      }
-
-      if (pageIndex + 1 == pageCount()) {
-          int diff = itemCount() % pageSize;
-          return diff == 0 ? pageSize : diff;
-      } else {
-          return pageSize;
-      }
-  }
-  
-  /**
-   * determines what page an item is on. Zero based indexes
-   * this method should return -1 for itemIndex values that are out of range
-   */
-  public int pageIndex(int itemIndex) {
-    if (itemIndex > itemCount() || itemIndex < 0 || itemCount() == 0) {
-        return -1;
+    /**
+     * The constructor takes in an array of items and a integer indicating how many
+     * items fit within a single page
+     */
+    public PaginationHelper(List<I> collection, int itemsPerPage) {
+        this.pageSize = itemsPerPage;
+        this.content = collection;
     }
-    int pageCounter = 0;
-    while (itemIndex > 0) {
-        itemIndex -= pageSize;
-        pageCounter +=1;
+
+    /**
+     * returns the number of items within the entire collection
+     */
+    public int itemCount() {
+        return content.size();
     }
-    return pageCounter - 1;
-  }
+
+    /**
+     * returns the number of pages
+     */
+    public int pageCount() {
+        int pageCount = itemCount() / pageSize;
+        return itemCount() % pageSize == 0 ? pageCount : pageCount + 1;
+    }
+
+    /**
+     * returns the number of items on the current page. page_index is zero based.
+     * this method should return -1 for pageIndex values that are out of range
+     */
+    public int pageItemCount(int pageIndex) {
+        pageIndex += 1;
+        if (pageIndex < 1 || itemCount() == 0 || pageIndex > pageCount()) {
+            return -1;
+        }
+
+        if (pageIndex == pageCount()) {
+            int diff = itemCount() % pageSize;
+            return diff == 0 ? pageSize : diff;
+        } else {
+            return pageSize;
+        }
+    }
+
+    /**
+     * determines what page an item is on. Zero based indexes
+     * this method should return -1 for itemIndex values that are out of range
+     */
+    public int pageIndex(int itemIndex) {
+        itemIndex += 1;
+        if (itemIndex > itemCount() || itemIndex < 0 || itemCount() == 0) {
+            return -1;
+        }
+        int pageCounter = 0;
+        while (itemIndex > 0) {
+            itemIndex -= pageSize;
+            pageCounter +=1;
+        }
+        return pageCounter - 1;
+    }
 }
