4e0d9d09b25ae7be7be959e6f764ec689afdf79a
Vihaan <32998032+TravelerVihaan@users.noreply.github.com>
2026-04-25 18:14:34 +0200

    PokerHand (#1)

--- src/main/java/com/github/vihaan/codewars/kyu4/PokerHand.java
+++ src/main/java/com/github/vihaan/codewars/kyu4/PokerHand.java
@@ -1,7 +1,16 @@
 package com.github.vihaan.codewars.kyu4;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /*
 A famous casino is suddenly faced with a sharp decline of their revenues. They decide to offer Texas hold'em also online. Can you help them by writing an algorithm that can rank poker hands?
@@ -40,41 +49,180 @@ Notes
 If you finished this kata, you might want to continue with Sortable Poker Hands
 
  */
-public class PokerHand
-{      
+public class PokerHand {
+
+    enum HandResult {
+        ROYAL_POKER(1),
+        POKER(2),
+        FOUR_OF_KIND(3),
+        FULL(4),
+        COLOR(5),
+        STREIGHT(6),
+        THREE_OF_A_KIND(7),
+        TWO_PAIRS(8),
+        ONE_PAIR(9),
+        HIGH_CARD(10);
+
+        final int handScore;
+
+        public int getHandScore() {
+            return handScore;
+        }
+
+        HandResult(int handScore) {
+            this.handScore = handScore;
+        }
+    }
+
     public enum Result { TIE, WIN, LOSS }
 
-    private final List<String> cards;
+    private final List<Card> cards;
 
-    PokerHand(String hand)
-    {
-        cards = Arrays.stream(hand.split(" ")).sorted().toList();
-    }
+//    private final Map<Integer, Long> cardsCounted;
+    private final Map<Long, List<Integer>> cardsCounted =  new HashMap<>();
 
-    public Result compareWith(PokerHand hand) {        
-        if (cards.equals(hand.getCards())) {
-            return Result.TIE;
+    private final boolean isMonocolored;
+    private HandResult result;
+    private int handResultScore;
+
+    private final int sumCardValues;
+
+    PokerHand(String hand) {
+        cards = Arrays.stream(hand.split(" ")).map(Card::of).sorted(Comparator.comparingInt(Card::getValue)).toList();
+        this.sumCardValues = cards.stream().mapToInt(Card::getValue).sum();
+        Map<Integer, Long> countedC = cards.stream().collect(Collectors.groupingBy(Card::getValue, Collectors.counting()));
+
+        countedC.forEach((k, v) -> cardsCounted.merge(v, new ArrayList<>(List.of(k)), (v1, v2) -> {
+            v1.addAll(v2);
+            return v1;
+        }));
+
+        this.isMonocolored = cards.stream().map(Card::getColor).distinct().count() == 1;
+        if (isMonocolored) {
+            setResult(HandResult.COLOR, cards.stream().max(Comparator.comparingInt(Card::getValue)).orElseThrow().getValue());
         }
-        return Result.TIE;
 
+        POKER_HAND_VALIDATORS.forEach(validator -> validator.accept(this));
     }
 
-//    private int calculateHandValue(List<String> cards) {
-//        var grouped = cards.stream().map(card -> card.substring(0,1)).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-//        var maxocurrences = grouped.entrySet()
-//                .stream()
-//                .max(Map.Entry.<String, Long>comparingByValue()).orElse(0);
-//
-//                //.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-//
-//
-//    }
+    void setResult(HandResult result, int handResultScore) {
+        if (this.result ==  null || this.result.getHandScore() > result.getHandScore()) {
+            this.result = result;
+            this.handResultScore = handResultScore;
+        }
+    }
 
-    public List<String> getCards() {
+    List<Card> getCards() {
         return cards;
     }
 
-    private static final int HAND_SIZE = 14;
-    private static final List<String> CARD_VALUES = List.of("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A");
-    private static final List<String> CARD_SUITS = List.of("S", "H", "D", "C");
+    public Map<Long, List<Integer>> getCardsCounted() {
+        return cardsCounted;
+    }
+
+    public Result compareWith(PokerHand hand) {
+        if (this.result.getHandScore() < hand.result.getHandScore()) {
+            return Result.WIN;
+        } else if (result.getHandScore() == hand.result.getHandScore()) {
+            if (this.handResultScore > hand.handResultScore) {
+                return Result.WIN;
+            } else if (this.handResultScore == hand.handResultScore) {
+                for (int i = HAND_SIZE - 1; i >= 0; i--) {
+                    if (this.cards.get(i).getValue() > hand.cards.get(i).getValue()) {
+                        return Result.WIN;
+                    } else if (this.cards.get(i).getValue() < hand.cards.get(i).getValue()) {
+                        return Result.LOSS;
+                    }
+                }
+                return Result.TIE;
+            } else {
+                return Result.LOSS;
+            }
+        }
+        return Result.LOSS;
+    }
+
+    static class Card {
+        private final int value;
+
+        private final char color;
+
+        public int getValue() {
+            return value;
+        }
+
+        public char getColor() {
+            return color;
+        }
+
+        private Card(int value, char color) {
+            this.value = value;
+            this.color = color;
+        }
+        private static Card of(String sCard) {
+            char chValue = sCard.charAt(0);
+            var val = switch (chValue) {
+                case 'T' -> 10;
+                case 'J' -> 11;
+                case 'Q' -> 12;
+                case 'K' -> 13;
+                case 'A' -> 14;
+                default -> Integer.parseInt(String.valueOf(chValue));
+            };
+            return new Card(val, sCard.charAt(1));
+        }
+    }
+
+    private static final int HAND_SIZE = 5;
+
+    private static final List<Consumer<PokerHand>> POKER_HAND_VALIDATORS = List.of(
+        PokerHand::pokerHandCheck,
+        PokerHand::checkFourOfAKind,
+        PokerHand::checkFullAndThreeOfAKind,
+        PokerHand::checkPairs,
+        PokerHand::checkHighCard
+    );
+
+    private static void checkHighCard(PokerHand pokerHand) {
+        pokerHand.setResult(HandResult.HIGH_CARD, pokerHand.getCards().stream().max(Comparator.comparingInt(Card::getValue)).orElseThrow().getValue());
+    }
+
+    static void pokerHandCheck(PokerHand pokerHand) {
+        if(IntStream.range(1, HAND_SIZE).allMatch(i -> pokerHand.getCards().get(i).getValue() - pokerHand.getCards().get(i - 1).getValue() == 1)) {
+            int handScore = pokerHand.getCards().stream().max(Comparator.comparingInt(Card::getValue)).orElseThrow().getValue();
+            if (pokerHand.isMonocolored) {
+                pokerHand.setResult(HandResult.POKER, handScore);
+                if (handScore == 14) {
+                    pokerHand.setResult(HandResult.ROYAL_POKER, handScore);
+                }
+            } else {
+                pokerHand.setResult(HandResult.STREIGHT, handScore);
+            }
+        }
+    }
+
+    private static void checkFourOfAKind(PokerHand pokerHand) {
+        Optional.ofNullable(pokerHand.getCardsCounted().get(4L)).ifPresent(e -> pokerHand.setResult(HandResult.FOUR_OF_KIND, e.get(0)));
+    }
+
+    private static void checkFullAndThreeOfAKind(PokerHand pokerHand) {
+        var triple = pokerHand.getCardsCounted().getOrDefault(3L, Collections.emptyList()).stream().findFirst().orElse(null);
+        if (triple != null && triple != 0) {
+            var pair = pokerHand.getCardsCounted().getOrDefault(2L, Collections.emptyList()).stream().findFirst().orElse(null);
+            if (pair != null && pair != 0) {
+                pokerHand.setResult(HandResult.FULL, triple);
+            } else {
+                pokerHand.setResult(HandResult.THREE_OF_A_KIND, triple);
+            }
+        }
+    }
+
+    private static void checkPairs(PokerHand pokerHand) {
+        var pairs = pokerHand.getCardsCounted().getOrDefault(2L, Collections.emptyList());
+        if (pairs.size() == 2) {
+            pokerHand.setResult(HandResult.TWO_PAIRS, pairs.stream().mapToInt(Integer::intValue).sum());
+        } else if (pairs.size() == 1) {
+            pokerHand.setResult(HandResult.ONE_PAIR, pairs.get(0));
+        }
+    }
 }
