83144444ddafacef50c276a8281db46bc1bd95bf
Vihaan <vihaan.1312@outlook.com>
2026-05-20 08:47:30 +0200

    EgyptianFractions

--- src/main/java/com/github/vihaan/codewars/kyu5/SomeEgyptianFractions.java
+++ src/main/java/com/github/vihaan/codewars/kyu5/SomeEgyptianFractions.java
@@ -2,136 +2,62 @@ package com.github.vihaan.codewars.kyu5;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
- * Given a rational number n
+ * Egyptian Fractions – greedy decomposition.
  *
- *  n >= 0, with denominator strictly positive
+ * Greedy step: for remaining n/d the smallest unit fraction <= n/d is 1/k
+ * where k = ceil(d/n) = (d + n - 1) / n.
  *
- *     as a string (example: "2/3" in Ruby, Python, Clojure, JS, CS, Go)
- *     or as two strings (example: "2" "3" in Haskell, Java, CSharp, C++, Swift, Dart)
- *     or as a rational or decimal number (example: 3/4, 0.67 in R)
- *     or two integers (Fortran)
- *
- * decompose this number as a sum of rationals with numerators equal to one and without repetitions (2/3 = 1/2 + 1/6 is correct but not 2/3 = 1/3 + 1/3, 1/3 is repeated).
- *
- * The algorithm must be "greedy", so at each stage the new rational obtained in the decomposition must have a denominator as small as possible. In this manner the sum of a few fractions in the decomposition gives a rather good approximation of the rational to decompose.
- *
- * 2/3 = 1/3 + 1/3 doesn't fit because of the repetition but also because the first 1/3 has a denominator bigger than the one in 1/2 in the decomposition 2/3 = 1/2 + 1/6.
- * Example:
- *
- * (You can see other examples in "Sample Tests:")
- *
- * decompose("21/23") or "21" "23" or 21/23 should return
- *
- * ["1/2", "1/3", "1/13", "1/359", "1/644046"] in Ruby, Python, Clojure, JS, CS, Haskell, Go, Scala
- *
- * "[1/2, 1/3, 1/13, 1/359, 1/644046]" in Java, CSharp, C++, Dart
- *
- * "1/2,1/3,1/13,1/359,1/644046" in C, Swift, R
- *
- * Notes
- *
- *     The decomposition of 21/23 as
- *
- *     21/23 = 1/2 + 1/3 + 1/13 + 1/598 + 1/897
- *
- *     is exact but don't fulfill our requirement because 598 is bigger than 359. Same for
- *
- *     21/23 = 1/2 + 1/3 + 1/23 + 1/46 + 1/69 (23 is bigger than 13)
- *     or
- *     21/23 = 1/2 + 1/3 + 1/15 + 1/110 + 1/253 (15 is bigger than 13).
- *
- *     The rational given to decompose could be greater than one or equal to one, in which case the first "fraction" will be an integer (with an implicit denominator of 1).
- *
- *     If the numerator parses to zero the function "decompose" returns [] (or "",, or "[]").
- *
- *     The number could also be a decimal which can be expressed as a rational.
- *
- * Example:
- *
- * 0.6 in Ruby, Python, Clojure,JS, CS, Julia, Go
- *
- * "66" "100" in Haskell, Java, CSharp, C++, C, Swift, Scala, Kotlin, Dart, ...
- *
- * 0.67 in R.
- *
- * Ref: http://en.wikipedia.org/wiki/Egyptian_fraction
+ * After subtracting: n/d - 1/k = (n*k - d) / (d*k)  →  simplify by GCD.
  */
 class SomeEgyptianFractions {
 
     public static String decompose(String nrStr, String drStr) {
-        Fraction fraction = Fraction.of(nrStr, drStr);
-        List<Integer> resultsDenominators = new ArrayList<>();
-        if (fraction.numerator == 0) {
-            return "[]";
-        } else if (fraction.denominator < fraction.numerator) {
-            return "[" + fraction.numerator / fraction.denominator + "]";
+        long fNumerator = Long.parseLong(nrStr);
+        long fDenominator = Long.parseLong(drStr);
+
+        List<String> parts = new ArrayList<>();
+
+        if (fNumerator == 0) {
+            return parts.toString();
         }
 
-        int currentDenominator = 2;
-        do {
-            if (fraction.numerator == 1) {
-                resultsDenominators.add(fraction.denominator);
-                break;
+        if (fNumerator > fDenominator) {
+            long whole = fNumerator / fDenominator;
+            fNumerator = fNumerator % fDenominator;
+            parts.add(String.valueOf(whole));
+            if (fNumerator == 0) {
+               return parts.toString();
             }
-            fraction = substractIfPossible(fraction, currentDenominator, resultsDenominators);
-            currentDenominator++;
-        } while (fraction.numerator != 0);
-        return "[" + resultsDenominators.stream()
-            .map(d -> "1/" + d)
-            .collect(Collectors.joining(", ")) + "]";
+        }
 
+        // Greedy loop
+        long egyptianFractionDen = 2;
+        while (fNumerator != 0) {
+            if ((double) fNumerator / fDenominator >= (double) 1 / egyptianFractionDen) {
+                long lcm = lcm(fDenominator, egyptianFractionDen);
+                fNumerator = fNumerator * (lcm / fDenominator) - (lcm / egyptianFractionDen);
+                long gcd = gcd(fNumerator, lcm);
+                fNumerator /= gcd;
+                fDenominator = lcm / gcd;
+                parts.add(String.format("1/%d", egyptianFractionDen));
+            }
+            egyptianFractionDen += 1;
+        }
+        return parts.toString();
     }
 
-    private static Fraction substractIfPossible(Fraction f, int denominatorToSubtract, List<Integer> resultsDenominators) {
-
-        var resultDenominator = f.denominator;
-        while (resultDenominator % denominatorToSubtract != 0) {
-            resultDenominator *= 2;
-        }
-
-        var resultNumerator = f.numerator * (resultDenominator / f.denominator);
-        var numeratorToSubtract = resultDenominator / denominatorToSubtract;
-
-        if (numeratorToSubtract > resultNumerator) {
-            return f;
-        }
-
-        resultNumerator = resultNumerator - numeratorToSubtract;
-
-        resultsDenominators.add(denominatorToSubtract);
-        int gcd = gcd(resultNumerator, resultDenominator);
-        return gcd != 0 ? Fraction.of(resultNumerator / gcd, resultDenominator / gcd) : Fraction.of(resultNumerator, resultDenominator);
-
-
+    private static long lcm(long denominator1, long denominator2) {
+        return Math.abs(denominator1 / gcd(denominator1, denominator2) * denominator2);
     }
 
-    private static int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-
-    private static class Fraction {
-        private final int numerator;
-        private final int denominator;
-
-        private Fraction(int numerator, int denominator) {
-            this.numerator = numerator;
-            this.denominator = denominator;
+    private static long gcd(long num1, long num2) {
+        while (num2 != 0) {
+            long divider = num1 % num2;
+            num1 = num2;
+            num2 = divider;
         }
-
-        private static Fraction of(String n, String d) {
-            return new Fraction(Integer.parseInt(n), Integer.parseInt(d));
-        }
-
-        private static Fraction of(int n, int d) {
-            return new Fraction(n,d);
-        }
-
-        @Override
-        public String toString() {
-            return numerator + "/" + denominator;
-        }
+        return num1;
     }
 }
--- src/test/java/com/github/vihaan/codewars/kyu5/SomeEgyptianFractionsTest.java
+++ src/test/java/com/github/vihaan/codewars/kyu5/SomeEgyptianFractionsTest.java
@@ -17,4 +17,15 @@ public class SomeEgyptianFractionsTest {
         testing(SomeEgyptianFractions.decompose("0", "2"), "[]");
         testing(SomeEgyptianFractions.decompose("9", "10"), "[1/2, 1/3, 1/15]");
     }
+
+    @Test
+    public void test2() {
+        assertEquals("[1/2, 1/4]",              SomeEgyptianFractions.decompose("3", "4"));
+        assertEquals("[3]",                      SomeEgyptianFractions.decompose("12", "4"));
+        assertEquals("[]",                       SomeEgyptianFractions.decompose("0", "2"));
+        assertEquals("[1/2, 1/3, 1/15]",         SomeEgyptianFractions.decompose("9", "10"));
+        assertEquals("[1/2, 1/3, 1/13, 1/359, 1/644046]",
+            SomeEgyptianFractions.decompose("21", "23"));
+        assertEquals("[1/2, 1/6]",               SomeEgyptianFractions.decompose("2", "3"));
+    }
 }
